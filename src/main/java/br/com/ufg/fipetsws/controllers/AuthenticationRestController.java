80a1c871307683a5a0463bea8feabc2ffa5f5d8c
Mario <mariohj94@gmail.com>
2020-01-02 22:00:03 -0300

    padrao retorno

--- src/main/java/br/com/ufg/fipetsws/controllers/AuthenticationRestController.java
+++ src/main/java/br/com/ufg/fipetsws/controllers/AuthenticationRestController.java
@@ -3,6 +3,7 @@ package br.com.ufg.fipetsws.controllers;
 import br.com.ufg.fipetsws.dto.UsuarioTokenDto;
 import br.com.ufg.fipetsws.entities.Usuario;
 import br.com.ufg.fipetsws.mappers.UsuarioMapper;
+import br.com.ufg.fipetsws.response.Response;
 import br.com.ufg.fipetsws.security.firebase.LoginResponse;
 import br.com.ufg.fipetsws.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,15 +39,18 @@ public class AuthenticationRestController {
 	UsuarioService usuarioService;
 
 	@PostMapping(value="/api/auth")
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthRequest authRequest) throws Exception{
+	public ResponseEntity<Response<UsuarioTokenDto>> createAuthenticationToken(@RequestBody AuthRequest authRequest) throws Exception{
+		Response<UsuarioTokenDto> response = new Response<UsuarioTokenDto>();
 		try {
 			Optional<Usuario> usuario = usuarioService.findByEmail(authRequest.getEmail());
 			LoginResponse loginAuth = authFirebaseService.autenticar(authRequest);
 			UsuarioTokenDto usuarioTokenDto = UsuarioMapper.INSTANCE.paraTokenDto(usuario.get());
 			usuarioTokenDto.setIdToken(loginAuth.getIdToken());
-			return ResponseEntity.ok(usuarioTokenDto);
+			response.setData(usuarioTokenDto);
+			return ResponseEntity.ok(response);
 		} catch (Exception e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.ok(response);
 		}
 	}
 
